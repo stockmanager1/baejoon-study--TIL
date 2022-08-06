10c913e346f97474917d37f13d20c580c6582006
kim young il <95357946+stockmanager1@users.noreply.github.com>
2022-08-06 14:54:38 +0900

    [Bronze V] Title: 시험 성적, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+if 90 <= n and n <= 100:
+  print('A')
+elif 80 <= n and n <= 89:
+  print('B')
+elif 70 <= n and n <= 79:
+  print('C')
+elif 60 <= n and n <= 69:
+  print('D')
+elif n <= 68:
+  print('F')
