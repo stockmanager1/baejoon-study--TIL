e97f2804b878e62cc273022d5638cd6bf944582f
kim young il <95357946+stockmanager1@users.noreply.github.com>
2023-01-09 15:47:27 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 616 ms, Memory: 261168 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -0,0 +1,20 @@
+n = int(input())
+list_A = input().split()
+list_A = list(map(int,list_A))
+    
+
+m = int(input())
+list_B = input().split()
+list_B = list(map(int,list_B))
+    
+from collections import Counter
+
+cnt = Counter(list_A)
+
+for i in range(len(list_B)):
+  if list_B[i] in cnt.keys():
+    list_B[i] = cnt[list_B[i]]
+  else:
+    list_B[i] = 0
+        
+print(*list_B)
