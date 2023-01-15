8496e048bd4ca79f18dad7f16ac166c363dc98cf
kim young il <95357946+stockmanager1@users.noreply.github.com>
2023-01-15 13:20:43 +0900

    [Bronze II] Title: 대표값2, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.py
@@ -0,0 +1,12 @@
+a = []
+a.append(int(input()))
+a.append(int(input()))
+a.append(int(input()))
+a.append(int(input()))
+a.append(int(input()))
+
+print(int(sum(a)/len(a)))
+
+a = sorted(a)
+
+print(a[2])
